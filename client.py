55d0300743981a66816727eb762e7674b97593ce
babyblue64 <abhay.raveendran2000@gmail.com>
2025-10-28 15:56:22 +0530

    fix: modify client.py to gracefully handle keyboard interrupt

--- client.py
+++ client.py
@@ -3,29 +3,26 @@ import websockets
 
 async def test_ws():
     uri = "ws://localhost:8000/ws"
+    async with websockets.connect(uri) as ws:
+        print("Connected to server\n")
+        try:
+            async for message in ws:
+                print(f"\n{message}")
+        except websockets.exceptions.ConnectionClosedOK:
+            print("Connection closed cleanly.")
+        except websockets.exceptions.ConnectionClosedError as e:
+            print("Connection closed with error:", e)
 
+
+async def main():
     try:
-        async with websockets.connect(uri) as ws:
-            print("Client: Connected to server")
+        await test_ws()
+    except asyncio.CancelledError:
+        print(" Connection closed cleanly.")
 
-            try:
-                while True:
-                    response = await ws.recv()
-                    print(f"\n{response}")
-            except websockets.exceptions.ConnectionClosed:
-                pass
 
-            except KeyboardInterrupt:
-                print(f"\nClient: Interrupted by user")
-
-            finally:
-                await ws.close()
-
-    except websockets.exceptions.ConnectionClosedOK:
-        print("Connection closed cleanly")
-    except websockets.exceptions.ConnectionClosedError as e:
-        print("Connection closed with error:", e)
-    except Exception as e:
-        print("Other error:", e)
-
-asyncio.run(test_ws())
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\Interrupted by user. Exiting gracefully...")
