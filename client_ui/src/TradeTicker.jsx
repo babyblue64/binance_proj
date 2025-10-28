e8db2e793671c520e8311abb1618a1ba1c019731
babyblue64 <abhay.raveendran2000@gmail.com>
2025-10-29 03:18:14 +0530

    fix: typo in TradeTicker.jsx

--- client_ui/src/TradeTicker.jsx
+++ client_ui/src/TradeTicker.jsx
@@ -1,6 +1,6 @@
 import  { useState, useEffect, useRef } from 'react';
 
-const WEBSOCKET_URL = "ws://localhost:8000/ws";
+const WEBSOCKET_URL = "wss://localhost:8000/ws";
 
 const TradeTicker = () => {
 
